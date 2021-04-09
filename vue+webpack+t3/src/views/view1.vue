ebe9747e3f4d4a3aad713da67fec23c256f06d89
linjiuzhou <ljz@szltech.com>
2021-04-09 15:43:41 +0800

    feat: threejs:book:

--- vue+webpack+t3/src/views/view1.vue
+++ vue+webpack+t3/src/views/view1.vue
@@ -1,12 +1,61 @@
 <template>
-  <div>view1111</div>
+  <div>
+    <header>view1</header>
+    <canvas id="three" ref="three"></canvas>
+  </div>
 </template>
 
 <script>
-export default {}
+import * as THREE from 'three'
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    init() {
+      this.createBaseThree()
+    },
+    createBaseThree() {
+      // 三大要素：scene, camera, render
+      // 给人的感觉threejs用起来就是个脚本，还是回去乖乖用html写吧
+      const scene = new THREE.Scene()
+      scene.background = new THREE.Color('#eee')
+      const canvas = this.$refs.three
+      // const canvas = document.querySelector('#three')
+      const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+      const camera = new THREE.PerspectiveCamera(
+        50,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      )
+      camera.position.z = 10
+
+      var boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+      var basicMaterial = new THREE.MeshBasicMaterial({ color: 0x0095dd })
+      var cube = new THREE.Mesh(boxGeometry, basicMaterial)
+      scene.add(cube)
+      cube.rotation.set(0.4, 0.2, 0)
+
+      function animate() {
+        renderer.render(scene, camera)
+        requestAnimationFrame(animate)
+      }
+      animate()
+    }
+  },
+  watch: {},
+  computed: {},
+  created() {},
+  mounted() {
+    this.init()
+  },
+  updated() {}
+}
 </script>
 <style scoped>
-div {
-  background: red;
+#three {
+  width: 500px;
+  height: 500px;
 }
 </style>
