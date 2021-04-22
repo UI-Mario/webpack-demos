bcaee6675ff4b85f1cd765a3ff9378d23a941896
linjiuzhou <ljz@szltech.com>
2021-04-22 18:42:50 +0800

    leave it

--- vue+webpack+t3/src/index.js
+++ vue+webpack+t3/src/index.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue' // Vue 3.x 引入 vue 的形式
-import App from './App.vue' // 引入 APP 页面组建
-import { router } from './router'
+// import { createApp } from 'vue' // Vue 3.x 引入 vue 的形式
+// import App from './App.vue' // 引入 APP 页面组建
+// import { router } from './router'
 
-const app = createApp(App) // 通过 createApp 初始化 app
-app.use(router)
-app.mount('#root') // 将页面挂载到 root 节点
+// const app = createApp(App) // 通过 createApp 初始化 app
+// app.use(router)
+// app.mount('#root') // 将页面挂载到 root 节点
 
 console.info('hello typescriptt')
 const obj = {
