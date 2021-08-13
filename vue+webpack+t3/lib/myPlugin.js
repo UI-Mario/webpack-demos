98e60e9aaccfe7dd7d47929ed3240a76f3c7886f
linjiuzhou <ljz@szltech.com>
2021-08-13 18:57:16 +0800

    chore: detail .babelrc

--- vue+webpack+t3/lib/myPlugin.js
+++ vue+webpack+t3/lib/myPlugin.js
@@ -6,8 +6,6 @@ class myPlugin {
   }
 
   apply(compiler) {
-    // 监听结束事件
-    // 难的学不会，简单的又瞧不上，回去写threejs了
     compiler.hooks.done.tap('myPlugin', (stats) => {
       this.doneCallback(stats)
     })
--- vue+webpack+t3/src/index.js
+++ vue+webpack+t3/src/index.js
@@ -1,27 +1,28 @@
-import { createApp } from 'vue' // Vue 3.x 引入 vue 的形式
-import App from './App.vue' // 引入 APP 页面组建
-import { router } from './router'
+// import { createApp } from 'vue' // Vue 3.x 引入 vue 的形式
+// import App from './App.vue' // 引入 APP 页面组建
+// import { router } from './router'
 
-const app = createApp(App) // 通过 createApp 初始化 app
-app.use(router)
-app.mount('#root') // 将页面挂载到 root 节点
-import test from './test.md'
-import ptest from './ptest.ptest'
-console.info('hello typescriptt')
+// // const app = createApp(App) // 通过 createApp 初始化 app
+// // app.use(router)
+// // app.mount('#root') // 将页面挂载到 root 节点
+// import test from './test.md'
+// import ptest from './ptest.ptest'
+// console.info('hello typescriptt')
 const obj = {
   a: 1,
   c: 3
 }
-console.log(obj?.c)
-console.log(null ?? 'this is null/undefined')
-console.log(test)
-console.log(ptest)
+// console.log(obj?.c)
+// console.log(null ?? 'this is null/undefined')
+// console.log(test)
+// console.log(ptest)
 
-import generatePlaceholder from '@findify/skeleton-generator'
+// import generatePlaceholder from '@findify/skeleton-generator'
 
-const placeholders = generatePlaceholder()
+// const placeholders = generatePlaceholder()
 
-console.log(placeholders)
+// console.log(placeholders)
+// console.log(process)
 
 // 这里自己给process.env增加了内容，详见bin文件夹
 // console.log(process.env)
