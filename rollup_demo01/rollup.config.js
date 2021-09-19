b25410a3e3991facd77afb4d001f7c9ccd7ec7c7
UI-Mario <JZ.Livioo@gmail.com>
2021-09-19 21:33:57 +0800

    rollup multi

--- rollup_demo01/rollup.config.js
+++ rollup_demo01/rollup.config.js
@@ -1,3 +1,4 @@
+// single entry
 export default {
   input: "main.js",
   output: {
@@ -5,3 +6,27 @@ export default {
     format: "cjs",
   },
 };
+
+// multi entry
+// export default [
+//     {
+//       input: 'main-a.js',
+//       output: {
+//         file: 'dist/bundle-a.js',
+//         format: 'cjs'
+//       }
+//     },
+//     {
+//       input: 'main-b.js',
+//       output: [
+//         {
+//           file: 'dist/bundle-b1.js',
+//           format: 'cjs'
+//         },
+//         {
+//           file: 'dist/bundle-b2.js',
+//           format: 'es'
+//         }
+//       ]
+//     }
+//   ];
