3ae0c9cb7b2838ee99777fb60d08423c1240c232
linjiuzhou <ljz@szltech.com>
2021-09-23 15:46:54 +0800

    stupid docs

--- demo-js/HelloWorldPlugin.js
+++ demo-js/HelloWorldPlugin.js
@@ -7,15 +7,20 @@ class HelloWorldPlugin {
   }
   apply(compiler) {
     // compiler hook:https://webpack.js.org/api/compiler-hooks/#hooks
-    // 所以，这个environment是干啥的，接在哪个后面，不知
-    compiler.hooks.environment.tap("hello world plugin", (stats) => {
+    // 所以，这些是干啥的，接在哪个后面，不知
+    // compilation: https://webpack.js.org/api/compilation-hooks/
+    // 入参stats：https://webpack.js.org/configuration/stats/
+    compiler.hooks.environment.tap("hello world plugin", () => {
       console.log('compiler hook environment');
     });
-    compiler.hooks.afterEnvironment.tap("hello world plugin", (stats) => {
+    compiler.hooks.afterEnvironment.tap("hello world plugin", () => {
       console.log('compiler hook afterEnvironment');
     });
     compiler.hooks.done.tap("hello world plugin", (stats) => {
-      console.log('compiler hook done');
+      // 都要吐血了这入参到底是啥
+      // 打印会报错，文档又不写
+      // f***
+      console.log('compiler hook done', stats.compilation.options);
     });
   }
 }
