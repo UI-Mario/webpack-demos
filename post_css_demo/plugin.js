3bfc5e82f21966522deb35faa5e821417d16ccc3
linjiuzhou <ljz@szltech.com>
2021-10-20 10:17:29 +0800

    post css demo

--- post_css_demo/plugin.js
+++ post_css_demo/plugin.js
@@ -1,5 +1,7 @@
 const postcss = require("postcss");
 
+// 列一下postcss所给出的数据类型
+
 module.exports = postcss.plugin("postcss-test-plugin", function (opts) {
   opts = opts || {};
   // Work with options here
@@ -7,6 +9,7 @@ module.exports = postcss.plugin("postcss-test-plugin", function (opts) {
     //   postcss好像把@media @keyframes给分成了AtRule对象，其他的是Rule对象
     console.log(root.nodes);
     // Transform CSS AST here
+    // 只是遍历Rule对象，还可能有注释Comment啥的
     root.walkRules(function (rule) {
       // We'll put more code here in a moment…
       //   Rule对象数据结构(只列出部分，详见https://postcss.org/api/#rule)
@@ -19,7 +22,12 @@ module.exports = postcss.plugin("postcss-test-plugin", function (opts) {
       //     source
       //     selector: css选择器, .class #id .class>.class之类的包括:hover，比较让我奇怪的是@media和@keyframes怎么获取呢
       // }
+      console.log(rule)
       rule.walkDecls(function (decl) {
+        // TODO: 比较迷惑的是，@media和@keyframe是AtRule对象，不是Rule对象
+        // 但是这里的decl又能拿到@keyframes里的css规则
+        // 就很迷惑
+        // console.log(decl.prop)
         // We work with each `decl` object here.
         // declaration 数据结构
         // {
