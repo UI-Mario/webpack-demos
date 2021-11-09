201b4f3128a0b11883b92aeb2bf1f1c406880f41
linjiuzhou <ljz@szltech.com>
2021-11-09 11:54:24 +0800

    add webpack chunk split config

--- vue+webpack+t3/webpack/webpack.prod.js
+++ vue+webpack+t3/webpack/webpack.prod.js
@@ -13,7 +13,37 @@ const prodConfig = {
         numiterations: 15
       }
     })
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [],
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
+  cache: {
+    type: "filesystem",
+    buildDependencies: {
+      config: [__filename], // 当构建依赖的config文件（通过 require 依赖）内容发生变化时，缓存失效
+    },
+    // 
+    name: "production-cache",
+  },
 }
 
 module.exports = merge(commonConfig, prodConfig)
